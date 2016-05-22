eb0f805b75549bbc8a82c9a2f56cbeb25e9fafa7
navneet <navneet@tyroo.(none)>
2016-05-22 18:45:47 +0530

    second part is commplete

--- scrapper-shopping.com/curlRequest.py
+++ scrapper-shopping.com/curlRequest.py
@@ -15,14 +15,41 @@ def itemCount(str):
     # such as standard output.
     # print(body.decode('iso-8859-1'))
     if not body.decode('iso-8859-1'):
-        print 'unable to parse the url'
+        print 'Unable to parse the url. Pleases try again.'
     else:
         str1 = 'quickLookItem'
-        totalItem = body.decode('iso-8859-1').count(str1)
+        totalItem = body.count(str1)
         print 'total relevent search item :', totalItem
 
 
     return
 
-itemCount("macbook");
+def itemCountPageWise(str,pageNumber):
+    import pycurl
+    from io import BytesIO
+
+    buffer = BytesIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, 'http://www.shopping.com/apple/products~PG-'+pageNumber+'?KW='+str)
+    c.setopt(c.WRITEFUNCTION, buffer.write)
+    c.perform()
+    c.close()
+
+    body = buffer.getvalue()
+    # Body is a byte string.
+    # We have to know the encoding in order to print it to a text file
+    # such as standard output.
+    #print(body.decode('iso-8859-1'))
+    if not body.decode('iso-8859-1'):
+        print 'Unable to parse the url. Pleases try again.'
+    else:
+        str1 = 'quickLookItem'
+        totalItem = body.count(str1)
+        print 'total relevent search item :', totalItem
+
+    return
+
+itemCount('macbook');
+itemCountPageWise('apple','4')
+
 
