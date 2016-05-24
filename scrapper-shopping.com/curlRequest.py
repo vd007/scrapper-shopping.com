a4d0a6909a4bf063c9a95207597d89cb40567906
navneet <navneet@tyroo.(none)>
2016-05-24 13:02:30 +0530

    test cases are added

--- scrapper-shopping.com/curlRequest.py
+++ scrapper-shopping.com/curlRequest.py
@@ -1,4 +1,5 @@
 import sys
+import unittest
 
 def itemCount(str):
     import pycurl
@@ -18,13 +19,14 @@ def itemCount(str):
     # print(body.decode('iso-8859-1'))
     if not body.decode('iso-8859-1'):
         print 'Unable to parse the url. Pleases try again.'
+	totalItem =0
     else:
         str1 = 'quickLookItem'
         totalItem = body.count(str1)
         print 'total relevent search item :', totalItem
 
 
-    return
+    return totalItem
 
 def itemCountPageWise(str,pageNumber):
     import pycurl
@@ -44,19 +46,33 @@ def itemCountPageWise(str,pageNumber):
     #print(body.decode('iso-8859-1'))
     if not body.decode('iso-8859-1'):
         print 'Unable to parse the url. Pleases try again.'
+	totalItem =0 
     else:
         str1 = 'quickLookItem'
         totalItem = body.count(str1)
         print 'total relevent search item :', totalItem
 
-    return
+    return totalItem
 
-#itemCount('macbook');
-#itemCountPageWise('macbook+pro','4')
+# variables for verify the test cases.
+function1 = itemCount('macbook');
+function2 = itemCountPageWise('macbook+pro','4')
+
+#function processing and returing the output as per the args
 argLength = len(sys.argv)
 if argLength == 2:
 	itemCount(sys.argv[1])
-else:
+elif argLength == 3:
 	itemCountPageWise(sys.argv[1],sys.argv[2])
+else:
+	print 'please provide correct arguement'
 
 
+############ Test Cases #########33
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(function1, 15)
+
+    def test1(self):
+        self.assertEqual(function2, 40)
